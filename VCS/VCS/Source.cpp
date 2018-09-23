afdefd3bd03e2f3e1057f02cb0f6bbf6d14edb10
Randy Le <97randy.le@gmail.com>
2018-09-23 00:43:28 -0700

    able to create sourcepath name folder to targetpath folder

--- VCS/VCS/Source.cpp
+++ VCS/VCS/Source.cpp
@@ -1,4 +1,4 @@
-//#define WINDOWS				/*uncomment this line to use it for windows*/
+#define WINDOWS				/*uncomment this line to use it for windows*/
 #ifdef WINDOWS
 #include "dirent.h"
 #include <direct.h>
@@ -18,21 +18,21 @@ using namespace std;
 
 void menu();
 void viewptree(struct dirent *sd, DIR *dir);
-void createrepo(string tp);
+void createrepo(string tp,string sp, struct dirent *sd, DIR *dir);
 
 int main(int argc, char* argv[]) {
 	int choice;
-	const string targetpath = "./repo";					//EDIT HERE FOR DESIRED TARGET PATH
+	const string targetpath = "./repo/";					//EDIT HERE FOR DESIRED TARGET PATH
 	const string sourcepath = "./ptree";				//EDIT HERE TO CHANGE SOURCE PATH
-	struct dirent *sd = NULL;
-	DIR *dir = opendir(sourcepath.c_str());
+	struct dirent *sd = NULL;							//sd is basically the current directory
+	DIR *dir = opendir(sourcepath.c_str());				//dir will be the dir we plan on copying
 
 	do {
 		menu();
 		cin >> choice;
 		switch (choice) {
 		case 1:
-			createrepo(targetpath);
+			createrepo(targetpath, sourcepath, sd, dir);
 			break;
 		case 2:
 			viewptree(sd, dir);
@@ -56,7 +56,7 @@ void menu() {
 }
 
 void viewptree(struct dirent *sd, DIR *dir) { 
-	dir = opendir("./ptree");
+	dir = opendir("./ptree");								//JUST MAKING IS SIMPLE SINCE WE KNEW WHERE THE SOURCE PATH IS
 	cout << "======================================\n";
 	if (dir == NULL) cout << "file does not exist\n";
 
@@ -68,10 +68,26 @@ void viewptree(struct dirent *sd, DIR *dir) {
 	closedir(dir);
 }
 
-void createrepo(string tp) {
+void createrepo(string tp, string sp, struct dirent *sd, DIR *dir) {
+	/* Checks if ptree exist and have it open and ready to copy to repository */
+	dir = opendir(sp.c_str());
+	if (dir == NULL) cout << sp << " directory was not found.\n";
+	//create the folder of the targetpath
+
 #if defined(_WIN32)
-	_mkdir(tp.c_str());
+	_chdir(tp.c_str());
+	cout << _getcwd(0, 64);
+	_mkdir("ptree");			//Create source project folder name
 #else
-	mkdir(tp.c_str(), 0777);
+	chdir(sp.c_str());
+	mkdir(tp.c_str());
 #endif
+
+	while ((sd = readdir(dir)) != NULL) {
+		if (!(strcmp(sd->d_name, ".")) || !(strcmp(sd->d_name, "..")))
+		{
+		}
+	}
+
+	closedir(dir);
 }
