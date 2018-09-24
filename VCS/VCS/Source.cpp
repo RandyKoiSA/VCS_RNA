dd1b734467ae4041dbad16207ab2f36bc83bc102
Randy Le <97randy.le@gmail.com>
2018-09-23 23:52:21 -0700

    adding oldSource

--- VCS/VCS/Source.cpp
+++ VCS/VCS/Source.cpp
@@ -10,21 +10,22 @@
 #include <sys/types.h>		//required to make files
 #include <sys/stat.h>		//required to make files
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <stdlib.h>  
-#include <stdio.h> 
+#include <stdio.h>
 
 using namespace std;
 
+
 void menu();
 void viewptree(struct dirent *sd, DIR *dir);
-void createrepo(string tp,string sp, struct dirent *sd, DIR *dir);
-void nextFile(struct dirent *sd, string path);
+void createrepo(string tp, string sp, struct dirent *sd, DIR *dir);
 
 int main(int argc, char* argv[]) {
 	int choice;
-	const string targetpath = "repo";					//EDIT HERE FOR DESIRED TARGET PATH
-	const string sourcepath = "ptree";				//EDIT HERE TO CHANGE SOURCE PATH
+	const string targetpath = "./repo/";					//EDIT HERE FOR DESIRED TARGET PATH
+	const string sourcepath = "./ptree";				//EDIT HERE TO CHANGE SOURCE PATH
 	struct dirent *sd = NULL;							//sd is basically the current directory
 	DIR *dir = opendir(sourcepath.c_str());				//dir will be the dir we plan on copying
 
@@ -56,14 +57,13 @@ void menu() {
 	cout << "\t0.Exit\n";
 }
 
-void viewptree(struct dirent *sd, DIR *dir) { 
+void viewptree(struct dirent *sd, DIR *dir) {
 	dir = opendir("./ptree");								//JUST MAKING IS SIMPLE SINCE WE KNEW WHERE THE SOURCE PATH IS
 	cout << "======================================\n";
 	if (dir == NULL) cout << "file does not exist\n";
 
 	while ((sd = readdir(dir)) != NULL) {
-		if ((!strcmp(sd->d_name, ".")) || (!strcmp(sd->d_name, ".."))) {}
-		else cout << sd->d_name << endl;
+		cout << sd->d_name << endl;
 	}
 	cout << "======================================\n";
 
@@ -71,72 +71,25 @@ void viewptree(struct dirent *sd, DIR *dir) {
 }
 
 void createrepo(string tp, string sp, struct dirent *sd, DIR *dir) {
-
-	//create the folder of the targetpath
-	string path;
-	string originalpath;
-#if defined(_WIN32)		
-	originalpath = _getcwd(0, 64);
-	_chdir(tp.c_str());				//go into repo folder		
-	_mkdir(sp.c_str());			   //create source folder in repo folder
-	path = _getcwd(0, 64);
-	cout << "This is where all the file will be copied at: " << path << endl;
-	_chdir(originalpath.c_str());
-	path = originalpath + "\\" + sp;
-	cout << "Back at : " << _getcwd(0, 64) << endl;
-#else
-	originalpath = getcwd(0, 64);
-	chdir(tp.c_str());
-	mkdir(sp.c_str());
-	path = getcwd(0, 64);
-	cout << "This is where all the file will be copied at: " << path << endl;
-	chdir(originalpath.c_str());
-	cout << "Back at : " << _getcwd(0, 64) << endl;
-	path = originalpath + sp;
-#endif
 	/* Checks if ptree exist and have it open and ready to copy to repository */
-	dir = opendir(sp.c_str());		//goes into ptree
+	dir = opendir(sp.c_str());
 	if (dir == NULL) cout << sp << " directory was not found.\n";
-	//SOMEHOW MAKE A RECURSION FUNCTION WITH THIS
-	//every readdir() should check if it's a .txt file, if so, create a copy of that file
+	//create the folder of the targetpath
+
+#if defined(_WIN32)
+	_chdir(tp.c_str());
+	cout << _getcwd(0, 64);
+	_mkdir("ptree");			//Create source project folder name
+#else
+	chdir(sp.c_str());
+	mkdir(tp.c_str(), 0777);
+#endif
+
 	while ((sd = readdir(dir)) != NULL) {
-		if ((!strcmp(sd->d_name, ".")) || (!strcmp(sd->d_name, ".."))) {}
-		else {
-			nextFile(sd, path);
+		if (!(strcmp(sd->d_name, ".")) || !(strcmp(sd->d_name, "..")))
+		{
 		}
 	}
 
 	closedir(dir);
-}
-
-void nextFile(struct dirent *sd, string path) {
-	string newPath;
-
-	DIR *dir = opendir(path.c_str());
-	if (dir == NULL) {cout << path << " could not be opened.\n"; }
-	else {
-		while ((sd = readdir(dir)) != NULL) {
-			if ((!strcmp(sd->d_name, ".")) || (!strcmp(sd->d_name, ".."))) {}
-			else {
-				newPath = path + "\\" + sd->d_name;
-#if defined (_WIN32)
-				cout << "Making " << sd->d_name << " directory...\n";
-				_mkdir(sd->d_name);
-				cout << "Changing directory to: " << newPath << endl;
-				_chdir(newPath.c_str());
-#else
-				mkdir(sd->d_name);
-				chdir(newPath.c_str());
-#endif
-				nextFile(sd, newPath);
-
-			}
-		}
-	}
-
-
-
-
-
-
 }
