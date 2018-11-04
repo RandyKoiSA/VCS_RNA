f08cd92c96561efbdf486798db8ae796d619d856
ayl02410 <ayl02410@users.noreply.github.com>
2018-11-03 17:31:16 -0700

    P1 & P2: check-out, are now functional
    Project 1 part is officially complete: created artifact ID & leave folder.
    Project 2: check-out is complete.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//  Core program functionalities and properties.
+//Core program functionalities and properties.
 //  Modify above line of description for every file modification.
 //  main.cpp
 //  Created by Alex Liao, Randy Le, Nathan Richards, and Dom Feeley on 2018/9/26.
@@ -17,15 +17,17 @@ using namespace boost::filesystem;
 
 boost::filesystem::fstream fileOp;
 
+const int multiplier[] = {1, 3, 7, 11, 17};
+
 const string cmdList[] = {"Exit", "View Project Source Directory", "View Project Repo Directory",
                           "Create Project Repository", "Project Check-Out", "Project Check-In"};
 
 ptime invalidTime = time_from_string("1900-01-01 08:00:00");
 
-/**
+/*
  Arglist struct
  grabs detail of command executed and add it into the manifest file.
- */
+*/
 struct ArgList {
   int cmd = -1;
   ptime cmdTime, repoTime;
@@ -37,10 +39,11 @@ void viewDirectory(path);
 void updateManifest(ArgList&);
 stringstream fileReader(path, ArgList&);
 void createRepo(path, path, ArgList&);
+string makeID(stringstream&);
 
-/**
+/*
  Main Method
- */
+*/
 int main(int argc, const char * argv[])
 {
   int choice, forks = 0;
@@ -73,22 +76,22 @@ int main(int argc, const char * argv[])
         args.manifestPath = projectPath.string() + "/Manifest.txt";
         if (invalidTime != args.repoTime) { updateManifest(args); }
         createRepo(projectPath, REPO_PATH, args);
-        copy_file(args.manifestPath, REPO_PATH.string() + "/Manifest.txt", boost::filesystem::detail::overwrite_if_exists);
+        copy_file(args.manifestPath, REPO_PATH.string() + "/Manifest.txt",
+                  boost::filesystem::detail::overwrite_if_exists);
         break;
       case 4:
         args.manifestPath = REPO_PATH.string() + "/Manifest.txt";
         updateManifest(args);
-        //create_directory(REPO_PATH/".."/ "VCS_Proj_Fork"+=to_string(forks += 1));
-        createRepo(REPO_PATH, REPO_PATH/".."/ "VCS_Proj_Fork"+=to_string(forks += 1), args);
+        createRepo(REPO_PATH, REPO_PATH /".."/ "VCS_Proj_Fork" += to_string(forks += 1), args);
         break;
     }
   } while (choice != 0);
   return 0;
 } // End of Main()
 
-/**
+/*
  Menu Method
- */
+*/
 void menu()
 {
   cout << "===============VCS CLI===============\n"
@@ -101,10 +104,10 @@ void menu()
   << "\tEnter choice: ";
 }
 
-/**
+/*
  viewDirectory Method
  @param   path stores the a directory path
- */
+*/
 void viewDirectory(path path)
 {
   // Print the content of the directory
@@ -117,16 +120,15 @@ void viewDirectory(path path)
 
 /*
  updateManifest Method
- 
  @param args stores action executed
- */
+*/
 void updateManifest(ArgList& args)
 {
   fileOp.open(args.manifestPath, ios::out | ios::app);
   
   if (invalidTime != args.cmdTime)
   {
-    fileOp << "*-> Command: " << cmdList[args.cmd] << " @ "<< args.cmdTime << "\n";
+    fileOp << "Command » " << cmdList[args.cmd] << " @ "<< args.cmdTime << "\n";
     args.repoTime = invalidTime;
   }
   else {
@@ -135,7 +137,7 @@ void updateManifest(ArgList& args)
       cout << "not open\n";
       cout << "args.path: " << args.filePath << endl;
     }
-    fileOp << "File: " << args.desc << " @ path: " << args.filePath << "\n";
+    fileOp << "   File » " << args.desc << " @ path = " << args.filePath << "\n";
   }
   fileOp.close();
 } // End of updateManifest(.)
@@ -143,15 +145,14 @@ void updateManifest(ArgList& args)
 
 /*
  fileReader Method
- 
  @param target
  @param args
- */
+*/
 stringstream fileReader(path target, ArgList& args)
 {
   stringstream strStream;
-  path relative;
-  if (invalidTime != args.cmdTime) { cout << "set args.cmdtime to invalid\n"; args.cmdTime = invalidTime; }
+
+//  if (invalidTime != args.cmdTime) { cout << "set args.cmdtime to invalid\n"; args.cmdTime = invalidTime; }
   
   if (".dylib" != target.extension()
       && "Manifest.txt" != target.filename() && "main" != target.filename())
@@ -165,24 +166,39 @@ stringstream fileReader(path target, ArgList& args)
     while(fileOp >> strStream.rdbuf());
     
     getline(strStream, desc);
-    args.desc = target.filename().string() + ". " + desc;
+    args.desc = target.filename().string() + ": " + desc;
+    
     fileOp.close();
-  } else {
-    args.desc = target.filename().string();
   }
-  relative = boost::filesystem::relative(target, "../");
-  args.filePath = relative.string();
-  updateManifest(args);
   return strStream;
 } // End of fileReader(..)
 
+
+/*
+ makeID
+ @param stringstream &stream
+*/
+string makeID(stringstream& stream)
+{
+  int ckSum = 0, counter = 0;
+  for (int ix = 0; ix != stream.str().size(); ++ix)
+  {
+    ckSum += int(stream.str()[ix]) * multiplier[counter];
+    ++counter;
+    if (5 == counter) { counter = 0; }
+  }
+  string id = to_string(ckSum) + "-L" + to_string(stream.str().size());
+  // <Missing: modulus operation on over sized cksum>
+  return id;
+} // End of makeID(.)
+
+
 /*
  createRepo Method
- 
  @param src
  @param target
  @param args
- */
+*/
 void createRepo(path src, path target, ArgList& args)
 {
   if (exists(target))
@@ -203,13 +219,46 @@ void createRepo(path src, path target, ArgList& args)
     }
     if (is_regular_file(entry) && entry.path().filename() != ".DS_Store")
     {
+      path relative;
       cout << "is file, path: " << target / entry.path().filename() << endl;
-//      if ("main.cpp" == entry.path().filename()) {
-//        create_directory(target / entry.path().filename());
-//      }
-      copy_file(entry, target / entry.path().filename());
-      stringstream strStream = fileReader(target / entry.path().filename(), args);
+      
+      if (invalidTime != args.cmdTime)
+      {
+        cout << "set args.cmdtime to invalid\n";
+        args.cmdTime = invalidTime;
+      }
+      if (".cpp" == entry.path().filename().extension() || "README.txt" == entry.path().filename())
+      {
+        stringstream strStream;
+        string id;
+        switch (args.cmd) {
+          case 4:
+            copy_file(entry, target / entry.path().filename());
+            break;
+          case 3:
+          case 5:
+            strStream = fileReader(entry, args);
+            //strStream = fileReader(target / entry.path().filename(), args);
+            id = makeID(strStream);
+            id += entry.path().extension().string();
+            cout << "id w/ ext.: " << id << endl;
+            path leave = target / entry.path().filename();
+            create_directory(leave);
+            fileOp.open(leave.string() / id, ios::out | ios::app);
+            fileOp << strStream.rdbuf();
+            fileOp.close();
+            break;
+        }
+      }
+      else
+      {
+        copy_file(entry, target / entry.path().filename());
+        args.desc = entry.path().filename().string();
+      }
+      relative = boost::filesystem::relative(target / entry.path().filename(), "../");
+      args.filePath = relative.string();
+      updateManifest(args);
     }
+    
   }
 } // End of createRepo(...)
-
